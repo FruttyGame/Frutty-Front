4f0e27a585b3315bd9c972681fd8a2094933e016
AbdulmajeedSM <m.almqbl@gmail.com>
2020-05-09 19:45:17 +0300

    Stack Nav

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
     <NavigationContainer />

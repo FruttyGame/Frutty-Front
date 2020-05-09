6fad549ec469891edb0f7dfed913e8f75a425d23
AbdulmajeedSM <m.almqbl@gmail.com>
2020-05-09 23:45:53 +0300

    install libraries

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ const Stack = createStackNavigator();
 function MyStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
     </Stack.Navigator>
   );
 }

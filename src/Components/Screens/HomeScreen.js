348a7548eabb8c85faad02984932fe66260e8622
AbdulmajeedSM <m.almqbl@gmail.com>
2020-05-10 02:07:15 +0300

    experiment with screen

--- src/Components/Screens/HomeScreen.js
+++ src/Components/Screens/HomeScreen.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import {StyleSheet,View,Text} from 'react-native';
+import {StyleSheet,View,Text,Image,ImageBackground} from 'react-native';
 
 export default function HomeScreen() {
 
         return (
             <View>
-            <Text>Let Start</Text>
+            <ImageBackground source={require('../../assets/images/background.png')} style={{width:'100%'}}>
+            <View>
+                <Image source={require('../../assets/images/gem1.png')}></Image>
+            </View>
+            </ImageBackground>
             </View>
         );
         const styles = StyleSheet.create({
+            Background: {
+                width:'100%',
+            },
             container: {
               flex: 1,
               backgroundColor: '#fff',

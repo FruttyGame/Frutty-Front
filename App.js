a5be890617b2a41230ac8b91693d5ce90a73d02a
AbdulmajeedSM <m.almqbl@gmail.com>
2020-05-09 19:43:20 +0300

    import libraries

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
   return (

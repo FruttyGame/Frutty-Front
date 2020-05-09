ba76540c0e6bc14b345d5a35caffdc0742a67e19
AbdulmajeedSM <m.almqbl@gmail.com>
2020-05-09 23:53:13 +0300

    add home

--- src/Components/Screens/HomeScreen.js
+++ src/Components/Screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View,Text} from 'react-native';
+import {StyleSheet,View,Text} from 'react-native';
 
 export default function HomeScreen() {
 
@@ -8,4 +8,12 @@ export default function HomeScreen() {
             <Text>Let Start</Text>
             </View>
         );
+        const styles = StyleSheet.create({
+            container: {
+              flex: 1,
+              backgroundColor: '#fff',
+              alignItems: 'center',
+              justifyContent: 'center',
+            },
+          });
 }

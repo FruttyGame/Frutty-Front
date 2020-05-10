8bdcd47930805bdfa52cd663a96ca2f9505c5d01
AbdulmajeedSM <m.almqbl@gmail.com>
2020-05-10 06:04:26 +0300

    add screen

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/Components/Screens/HomeScreen.js';
 
 
 const Stack = createStackNavigator();
--- src/Components/Screens/HomeScreen.js
+++ src/Components/Screens/HomeScreen.js
@@ -5,16 +5,7 @@ import {
   Text,
   TextInput,
   View,
-  TouchableHighlight,
-  ActivityIndicator
 } from 'react-native';
-
-import { LoginButton } from 'react-native-fbsdk';
-
-import { Actions, ActionConst } from 'react-native-router-flux';
-
-
-import styles from '../styles/styles';
 
 
 export default class HomeScreen extends Component {

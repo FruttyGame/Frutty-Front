c93c9f4dfe643bacf3c13883484866eb24079754
AbdulmajeedSM <m.almqbl@gmail.com>
2020-05-11 20:23:03 +0300

    Login Screen

--- Components/Screens/Login/Login.js
+++ Components/Screens/Login/Login.js
@@ -1,36 +1,3 @@
-import React,{Component} from 'react';
-import LoginForm from './LoginForm.js';
-import {View,Image,StyleSheet} from 'react-native';
+import React from 'react';
+import {ScrollView, StyleSheet, View, KeyboardAvoidingView} from 'react-native';
 
-class Login extends React.Component {
-    render() {
-        return (
-<View style={styles.container}>
-        <View style={styles.loginContainer}>
-                    <Image resizeMode="contain" style={styles.logo} source={require('../../../assets/MYLOGO.png')} />
-         </View>
-            <View style={styles.formContainer}>
-                   <LoginForm />
-            </View>
-       </View>
-        );
-    }
-}
-// define your styles
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#2c3e50',
-    },
-    loginContainer:{
-        alignItems: 'center',
-        flexGrow: 1,
-        justifyContent: 'center'
-    },
-    logo: {
-        position: 'absolute',
-        width: 300,
-        height: 100
-    }
-});
-export default Login;

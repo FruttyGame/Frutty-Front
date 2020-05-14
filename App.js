d3ad45a24fa24d6b2cc285d255fb6cab8630c3a6
AbdulmajeedSM <m.almqbl@gmail.com>
2020-05-14 19:29:11 +0300

    commit

--- App.js
+++ App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import 'react-native-gesture-handler';
-import { StyleSheet, Text, View } from 'react-native';
-import MainNavigator from './Navigator'
+import React, { useState } from 'react';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+import ReduxThunk from 'redux-thunk';
+import MainNavigator from './Navigator';
 
-
-
-class App extends React.Component {
-
-  render(){
-  return (
-    <MainNavigator />
-  );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const rootReducer = combineReducers({
+  products: productsReducer,
+  cart: cartReducer,
+  orders: ordersReducer
 });
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
+
+export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setFontLoaded(true);
+        }}
+      />
+    );
+  }
+  return (
+    <Provider store={store}>
+      <MainNavigator />
+    </Provider>
+  );
+}

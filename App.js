e6aaf1c589820939ff2b009e973e8c0251e26e9d
AbdulmajeedSM <m.almqbl@gmail.com>
2020-05-18 14:38:53 +0300

    Changes

--- App.js
+++ App.js
@@ -6,21 +6,6 @@ import * as Font from 'expo-font';
 import ReduxThunk from 'redux-thunk';
 import MainNavigator from './Navigator';
 
-const rootReducer = combineReducers({
-  products: productsReducer,
-  cart: cartReducer,
-  orders: ordersReducer
-});
-
-const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
-
-const fetchFonts = () => {
-  return Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
-  });
-};
-
 export default function App() {
   const [fontLoaded, setFontLoaded] = useState(false);
 

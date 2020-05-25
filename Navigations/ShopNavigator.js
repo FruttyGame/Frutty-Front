ac418aad192b293a7e17890f83bd32658cfb325d
AbdulmajeedSM <m.almqbl@gmail.com>
2020-05-25 15:24:40 +0300

    Auth Nav

--- Navigations/ShopNavigator.js
+++ Navigations/ShopNavigator.js
@@ -8,3 +8,36 @@ import { Platform, SafeAreaView, Button, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useDispatch } from 'react-redux';
 
+import AuthScreen, {
+    screenOptions as authScreenOptions
+  } from '../screens/user/AuthScreen';
+  import StartupScreen from '../screens/StartupScreen';
+  import Colors from '../constants/Colors';
+  import * as authActions from '../store/actions/auth';
+  
+  const defaultNavOptions = {
+    headerStyle: {
+      backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
+    },
+    headerTitleStyle: {
+      fontFamily: 'open-sans-bold'
+    },
+    headerBackTitleStyle: {
+      fontFamily: 'open-sans'
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
+  };
+
+  const AuthStackNavigator = createStackNavigator();
+
+export const AuthNavigator = () => {
+  return (
+    <AuthStackNavigator.Navigator screenOptions={defaultNavOptions}>
+      <AuthStackNavigator.Screen
+        name="Auth"
+        component={AuthScreen}
+        options={authScreenOptions}
+      />
+    </AuthStackNavigator.Navigator>
+  );
+};

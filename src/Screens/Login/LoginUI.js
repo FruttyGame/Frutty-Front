6f79a50b32c5b688183582ebb58a391137df9cbe
AbdulmajeedSM <m.almqbl@gmail.com>
2020-05-27 15:52:51 +0300

    Login UI

--- src/Screens/Login/LoginUI.js
+++ src/Screens/Login/LoginUI.js
@@ -9,20 +9,7 @@ class LoginUI extends Component {
         <View>
             <Text>LoginUI</Text>
             <Button title="Click me" onPress={() => this.props.navigation.navigate("HomeScreen")} />
-            <LoginButton
-          publishPermissions={["email"]}
-          onLoginFinished={
-            (error, result) => {
-              if (error) {
-                alert("Login failed with error: " + error.message);
-              } else if (result.isCancelled) {
-                alert("Login was cancelled");
-              } else {
-                alert("Login was successful with permissions: " + result.grantedPermissions)
-              }
-            }
-          }
-          onLogoutFinished={() => alert("User logged out")}/>
+            
 
         </View>
     );

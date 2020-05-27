13e57dd814d1a2145a305c464b1c7db07a0b0ecd
AbdulmajeedSM <m.almqbl@gmail.com>
2020-05-27 12:10:43 +0300

    Setup Stack

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import LoginUI from './src/Screens/LoginUI';
 import { View, Text } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer, StackActions} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 const stack = createStackNavigator();
 
 export default function App() {
  
+  createHomeStack = () =>
+    <Stack.Navigator>
+    <Stack.Screen />
+    </Stack.Navigator>
+  
   return (
-    <NavigationContainer></NavigationContainer>
+    <NavigationContainer>
+
+    </NavigationContainer>
   );
 }

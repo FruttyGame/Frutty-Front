a133ddd9fd9e629f1fd91e621b3c566053e6ac1c
AbdulmajeedSM <m.almqbl@gmail.com>
2020-05-27 12:04:20 +0300

    Setup loginUI

---
+++ src/Screens/LoginUI.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+const LoginUI = () => {
+
+    return (
+        <View>
+            <Text>LoginUI</Text>
+        </View>
+    );
+
+}
+
+export default LoginUI;
